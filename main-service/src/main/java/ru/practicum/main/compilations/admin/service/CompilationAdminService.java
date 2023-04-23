317e438a749789abf8dd3d4695390c7d39a7ff94
avbekker <abekker999@yandex.ru>
2023-04-23 23:31:16 +0600

    feat: compilation admin implemented

---
+++ main-service/src/main/java/ru/practicum/main/compilations/admin/service/CompilationAdminService.java
@@ -0,0 +1,13 @@
+package ru.practicum.main.compilations.admin.service;
+
+import ru.practicum.main.compilations.dto.CompilationDto;
+import ru.practicum.main.compilations.dto.NewCompilationDto;
+import ru.practicum.main.compilations.dto.UpdateCompilationRequest;
+
+public interface CompilationAdminService {
+    CompilationDto create(NewCompilationDto newCompilationDto);
+
+    CompilationDto update(Long compilationId, UpdateCompilationRequest compilation);
+
+    void delete(Long compilationId);
+}
---
+++ main-service/src/main/java/ru/practicum/main/compilations/admin/service/CompilationAdminServiceImpl.java
@@ -0,0 +1,26 @@
+package ru.practicum.main.compilations.admin.service;
+
+import org.springframework.stereotype.Service;
+import ru.practicum.main.compilations.dto.CompilationDto;
+import ru.practicum.main.compilations.dto.NewCompilationDto;
+import ru.practicum.main.compilations.dto.UpdateCompilationRequest;
+
+@Service
+public class CompilationAdminServiceImpl implements CompilationAdminService {
+
+
+    @Override
+    public CompilationDto create(NewCompilationDto newCompilationDto) {
+        return null;
+    }
+
+    @Override
+    public CompilationDto update(Long compilationId, UpdateCompilationRequest compilation) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long compilationId) {
+
+    }
+}
