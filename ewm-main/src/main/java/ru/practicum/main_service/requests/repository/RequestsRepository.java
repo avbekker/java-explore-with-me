f264187df2e89a5e5e7b369bf02b6fdd91806ba6
avbekker <abekker999@yandex.ru>
2023-04-27 00:06:34 +0600

    fix: many bugs

--- ewm-main/src/main/java/ru/practicum/main_service/requests/repository/RequestsRepository.java
+++ ewm-main/src/main/java/ru/practicum/main_service/requests/repository/RequestsRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.main_service.requests.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.practicum.main_service.enums.Status;
 import ru.practicum.main_service.events.model.Event;
 import ru.practicum.main_service.requests.model.Request;
@@ -18,14 +17,7 @@ public interface RequestsRepository extends JpaRepository<Request, Long> {
 
     Optional<Request> findByRequesterAndId(User requester, Long id);
 
-    @Query("select count(r.id) from Request r where r.event = ?1 and r.status = ?2")
-    Long findCount(Event event, Status status);
-
     List<Request> findByEventAndStatus(Event event, Status status);
-
-    List<Request> findByEventAndStatusIn(Event event, List<Status> status);
-
-    List<Request> findByEventInAndStatus(List<Event> events, Status status);
 
     List<Request> findByEvent(Event events);
 
